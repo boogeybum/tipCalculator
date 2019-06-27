f6a15d55e44e34db0845dab226fa76f0c7e992bd
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-27 01:35:03 -0500

    Link to bootstrap and basic layout set

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,8 @@ function tipCalc(val1, val2, val3) {
   var tipAmount = val1 * percentage;
   var total = val1 + tipAmount;
   var totalPerPerson = total / val3;
-  console.log("The total with tip is " + total.toFixed(2));
-  console.log("The total per person is " + totalPerPerson.toFixed(2));
+  console.log('The total with tip is ' + total.toFixed(2));
+  console.log('The total per person is ' + totalPerPerson.toFixed(2));
 }
 
 tipCalc(subtotal, tipPercent, numPeople);
