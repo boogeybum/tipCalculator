d44b95521fc9322b4ae8240f19c3b03c6b628dc0
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-27 01:56:31 -0500

    Added submit button to html and display total values on page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,19 @@
-var subtotal = 125;
-var tipPercent = 15;
-var numPeople = 4;
+const subtotal = 125;
+const tipPercent = 15;
+const numPeople = 4;
 
 function tipCalc(val1, val2, val3) {
-  var percentage = val2 / 100;
-  var tipAmount = val1 * percentage;
-  var total = val1 + tipAmount;
-  var totalPerPerson = total / val3;
+  const percentage = val2 / 100;
+  const tipAmount = val1 * percentage;
+  const total = val1 + tipAmount;
+  const totalPerPerson = total / val3;
+
   console.log('The total with tip is ' + total.toFixed(2));
   console.log('The total per person is ' + totalPerPerson.toFixed(2));
+  
+  document.getElementById("totalWithTip").innerHTML = total.toFixed(2);
+  document.getElementById("totalPerPerson").innerHTML = totalPerPerson.toFixed(2);
+
 }
 
 tipCalc(subtotal, tipPercent, numPeople);
