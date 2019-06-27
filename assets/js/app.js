741e0c873c611e2d0846fd7fcffe50a0f9721128
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-27 00:45:32 -0500

    basic tip calculator function complete

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+var subtotal = 125;
+var tipPercent = 15;
+var numPeople = 4;
+
+function tipCalc(val1, val2, val3) {
+  var percentage = val2 / 100;
+  var tipAmount = val1 * percentage;
+  var total = val1 + tipAmount;
+  var totalPerPerson = total / val3;
+  console.log("The total with tip is " + total.toFixed(2));
+  console.log("The total per person is " + totalPerPerson.toFixed(2));
+}
+
+tipCalc(subtotal, tipPercent, numPeople);
